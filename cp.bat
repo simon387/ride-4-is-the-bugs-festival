1602bc467d070538761c479394055973feda62bd
simone.celia <simone.celia@eng.it>
2024-01-02 09:55:13 +0100

    $random_string

--- cp.bat
+++ cp.bat
@@ -1,15 +1,25 @@
 @echo off
 setlocal
 
+:: Variables
 set "srcDir=C:\Users\ThinkOpen\AppData\Local\Ride4"
 set "toDir=C:\dev\ride-4-is-the-bugs-festival\bu\Ride4"
 
-:: Verifica se la cartella di destinazione esiste già, altrimenti la crea
+:: Check if the destination folder already exists, otherwise create it
 if not exist "%toDir%" mkdir "%toDir%"
 
-:: Sposta la cartella e sovrascrive tutto
+:: Move the directory and overwrite everything
 xcopy /y /s /e "%srcDir%" "%toDir%"
 
 echo Copy Done.
+
+:: Generate a random string
+random_string=$(openssl rand -hex 16)
+
+git add .
+git commit -m "$random_string"
+git push
+
+echo Commit Done.
 
 endlocal
